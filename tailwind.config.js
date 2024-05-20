23afd70775368c30ed8909b11861554a4aac7722
0xbarbs <0xbarbs@proton.me>
2024-05-20 21:33:56 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": { max: "1400px" },
+        xl: { max: "1279px" },
+        lg: { max: "1023px" },
+        md: { max: "767px" },
+        sm: { max: "639px" },
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
