68a4922311508b6238563a9b72f3035f958209d9
0xbarbs <0xbarbs@proton.me>
2024-06-28 13:53:55 +0100

    fix: changes to theme color and icons

--- vite.config.ts
+++ vite.config.ts
@@ -22,15 +22,15 @@ export default defineConfig({
         name: "Beta Credit Club",
         short_name: "CreditClub",
         description: "The first credit club filled with the most trustworthy people, helping to test trustless trust.",
-        theme_color: "#f983a3",
+        theme_color: "#fafafa",
         icons: [
           {
-            src: "/icons/icon-192x192.png",
+            src: "/icons/192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "/icons/512x512.png",
             sizes: "512x512",
             type: "image/png"
           }
