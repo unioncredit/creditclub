583d58e5c52ce1c06b8d92ccac0d4561b22812cf
0xbarbs <0xbarbs@proton.me>
2024-07-24 22:31:23 +0100

    feat: default to farcaster names and avatars if ens is not set

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,9 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import "@/polyfills";
 import App from './App.tsx'
+import { init } from "@airstack/airstack-react";
+
+init(import.meta.env.VITE_AIRSTACK_API_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
