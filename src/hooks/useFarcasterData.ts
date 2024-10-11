3ba29ec3610bfd7bc74e9e18d6cda31174535e9c
barbs <115395121+0xbarbs@users.noreply.github.com>
2024-10-11 13:43:08 +0100

    UNI-2057 Implement farcaster data for primary label components #33

---
+++ src/hooks/useFarcasterData.ts
@@ -0,0 +1,36 @@
+import { useLazyQuery } from "@airstack/airstack-react";
+import { useEffect } from "react";
+import { Address } from "viem";
+
+const query = `
+query RetrieveFarcasterData ($address: Address!) {
+  Socials(
+    input: {blockchain: ethereum, filter: {userAssociatedAddresses: {_eq: $address}, dappName: {_eq: farcaster}}}
+  ) {
+    Social {
+      name: profileName
+      bio: profileBio
+    }
+  }
+}
+`;
+
+export const useFarcasterData = (address: Address) => {
+  // We have to use lazy loading and manual fetching because of a bug in the useQuery function that doesn't detect changes in our addresses variable
+  const [fetch, { data: response, loading, error }] = useLazyQuery(
+    query,
+    { address: address.toLowerCase() },
+    { cache: true }
+  );
+
+  useEffect(() => {
+    fetch();
+  }, [fetch, address]);
+
+  const data = response?.Socials?.Social?.[0] || {
+    name: null,
+    bio: null,
+  };
+
+  return { data, error, loading };
+};
--- src/hooks/usePrimaryLabel.ts
+++ src/hooks/usePrimaryLabel.ts
@@ -1,6 +1,7 @@
 import { Address, zeroAddress } from "viem";
 import { useEns } from "@/hooks/useEns.ts";
 import { truncateAddress, truncateEns } from "@/utils/format.ts";
+import { useFarcasterData } from "@/hooks/useFarcasterData.ts";
 
 export const usePrimaryLabel = ({
  address,
@@ -11,10 +12,12 @@ export const usePrimaryLabel = ({
   shouldTruncate?: boolean;
   defaultValue?: string;
 }) => {
-  const { name } = useEns(address || zeroAddress);
+  const { name: ens } = useEns(address || zeroAddress);
+  const { data: farcasterData } = useFarcasterData(address || zeroAddress);
+  const { name: fname } = farcasterData;
 
   return {
-    data: address ? (name && (shouldTruncate ? truncateEns(name) : name)) || defaultValue ||
-      (shouldTruncate ? truncateAddress(address) : address) : defaultValue
+    data: fname || (address ? (ens && (shouldTruncate ? truncateEns(ens) : ens)) || defaultValue ||
+      (shouldTruncate ? truncateAddress(address) : address) : defaultValue)
   };
 }
