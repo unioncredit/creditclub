582e841a4b907403732b2500f388b237e6d88a52
0xbarbs <0xbarbs@proton.me>
2024-10-18 17:37:57 +0100

    feat: use nft artwork for club mint image

--- src/components/member/MembershipPerks.tsx
+++ src/components/member/MembershipPerks.tsx
@@ -4,14 +4,15 @@ import React from "react";
 import {
   Text,
   Heading,
+  Skeleton,
   // @ts-ignore
 } from "@unioncredit/ui";
-
-import NftImage from "@/assets/nft-prevew.svg";
 
 import { format, formattedNumber } from "@/utils/format.ts";
 import { useMemberCredit } from "@/hooks/useMemberCredit.ts";
 import { useVesting } from "@/hooks/useVesting.ts";
+import { useNftInfo } from "@/hooks/useNftInfo.ts";
+import { generateIpfsLink } from "@/utils/links.ts";
 
 export const MembershipPerks = ({
   title = "BCC Membership",
@@ -22,6 +23,7 @@ export const MembershipPerks = ({
   subtitle?: string;
   additionalPerks?: string[]
 }) => {
+  const { name, image } = useNftInfo();
   const { new: creditPerMember } = useMemberCredit();
   const { data: vestingData } = useVesting();
   const { startingPercentage, duration } = vestingData;
@@ -29,7 +31,11 @@ export const MembershipPerks = ({
   return (
     <div className="MembershipPerks">
       <div className="MembershipPerks__image">
-        <NftImage />
+        {image ? (
+          <img src={generateIpfsLink(image)} alt={name} />
+        ) : (
+          <Skeleton width="inherit" height="inherit" grey={200} shimmer />
+        )}
       </div>
       <div className="MembershipPerks__content">
         <Heading level={3}>{title}</Heading>
