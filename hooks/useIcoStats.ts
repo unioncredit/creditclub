c03fad8ae2dbe16d84fbd5ea41d3205c9070b944
0xbarbs <0xbarbs@proton.me>
2025-02-24 17:59:45 +0000

    fix: use asset token decimals when calculating ico stats

--- hooks/useIcoStats.ts
+++ hooks/useIcoStats.ts
@@ -1,16 +1,16 @@
 import { Address } from "viem";
 import { useClubData } from "@/hooks/useClubData";
 import { formatDecimals } from "@/lib/format";
+import { useErc20Token } from "@/hooks/useErc20Token";
 
 export const useIcoStats = (clubAddress: Address) => {
   const result = useClubData(clubAddress);
   const { data } = result;
 
-  const {
-    initialRaise,
-    decimals,
-    totalAssets
-  } = data;
+  const { data: assetToken } = useErc20Token(data.assetAddress);
+
+  const { initialRaise, totalAssets } = data;
+  const { decimals } = assetToken;
 
   const percentage = initialRaise > 0n ?
     Math.min(Math.max(Number((totalAssets * BigInt(100)) / initialRaise), 0), 100)
