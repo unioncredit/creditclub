d24f309191cee3dc3e171546d3500e6796b4a2fc
0xbarbs <0xbarbs@proton.me>
2025-03-12 15:34:00 +0000

    feat: use alchemy rpcs

--- providers/Web3Provider.tsx
+++ providers/Web3Provider.tsx
@@ -1,17 +1,18 @@
 import { createConfig, WagmiProvider } from "@privy-io/wagmi";
-import { base, mainnet, optimism } from "viem/chains";
+import { base, Chain, mainnet, optimism } from "viem/chains";
 import { http } from "viem";
 import React from "react";
 import { cookieStorage, createStorage } from "wagmi";
 
+import { RPC_URL, rpcChains } from "@/constants";
+
 export const config = createConfig({
   ssr: true,
   chains: [base, mainnet, optimism],
-  transports: {
-    [base.id]: http(),
-    [mainnet.id]: http(),
-    [optimism.id]: http(),
-  },
+  transports: rpcChains.reduce((acc: any, network: Chain) => ({
+    ...acc,
+    [network.id]: http(RPC_URL(network.id)),
+  }), {}),
   storage: createStorage({
     storage: cookieStorage,
   }),
