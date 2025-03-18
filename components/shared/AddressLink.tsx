6468ba49d1152eab0321dea14e63107573e5d81d
barbs <115395121+0xbarbs@users.noreply.github.com>
2025-03-18 14:38:30 +0000

    UNI-2345 Link to base profiles instead of optimism #74

--- components/shared/AddressLink.tsx
+++ components/shared/AddressLink.tsx
@@ -5,7 +5,7 @@ import { PrimaryLabel } from "@/components/shared/PrimaryLabel";
 export const AddressLink = ({ address }: { address: Address; }) => {
   return (
     <span>
-      <a target="_blank" rel="noopener" href={`https://app.union.finance/profile/opt:${address}`}>
+      <a target="_blank" rel="noopener" href={`https://app.union.finance/profile/base:${address}`}>
         <PrimaryLabel address={address} />
       </a>
     </span>
