de9c287e81cae3afa0d9d2e6bfc21aea0af666f3
barbs <115395121+0xbarbs@users.noreply.github.com>
2025-03-24 14:59:42 +0000

    UNI-2301 Mint and redeem modal improvements #81

--- components/modals/MintRedeemModal.tsx
+++ components/modals/MintRedeemModal.tsx
@@ -1,32 +1,12 @@
-import {
-  Button,
-  Modal,
-  ModalOverlay,
-  Input,
-  SegmentedControl,
-  InfoBanner,
-  CalendarIcon,
-  WalletIcon,
-  // @ts-ignore
-} from "@unioncredit/ui";
-
-import { useModals } from "@/providers/ModalManagerProvider";
-import { SendReceivePanel } from "@/components/shared/SendReceivePanel";
+// @ts-ignore
+import { Modal, ModalOverlay, SegmentedControl } from "@unioncredit/ui";
+import { Address } from "viem";
 import { useState } from "react";
+
 import { useClubData } from "@/hooks/useClubData";
-import { Address, erc20Abi } from "viem";
-import { useAccount, useWatchAsset } from "wagmi";
-import { useClubMember } from "@/hooks/useClubMember";
-import { useErc20Token } from "@/hooks/useErc20Token";
-import { formatDecimals } from "@/lib/format";
-import { IFormField, IFormValues, useForm } from "@/hooks/useForm";
-import { useMintRedeemPreview } from "@/hooks/useMintRedeemPreview";
-import { ApprovalButton } from "@/components/shared/ApprovalButton";
-import { useCreditVaultContract } from "@/hooks/useCreditVaultContract";
-import { useWrite } from "@/hooks/useWrite";
-import { useClubActivation } from "@/hooks/useClubActivation";
-import { formatDuration } from "@/lib/utils";
-import { POST_TX_MODAL } from "@/components/modals/PostTxModal";
+import { MintPanel } from "@/components/modals/panels/MintPanel";
+import { useModals } from "@/providers/ModalManagerProvider";
+import { RedeemPanel } from "@/components/modals/panels/RedeemPanel";
 
 export const MINT_REDEEM_MODAL = "mint-redeem-modal";
 
@@ -39,135 +19,12 @@ export const MintRedeemModal = ({
 }) => {
   const [tab, setTab] = useState<"mint" | "redeem">(activeTab);
 
-  const { open: openModal, close } = useModals();
-  const { address } = useAccount();
-  const { data: clubData, refetch: refetchClubData } = useClubData(clubAddress)
-  const { data: clubMember, refetch: refetchClubMember } = useClubMember(address, clubAddress);
-  const { data: assetToken } = useErc20Token(clubData.assetAddress);
-  const { activated, locked, remaining } = useClubActivation(clubAddress);
-  const { watchAsset } = useWatchAsset();
+  const { close } = useModals();
+  const { data: clubData } = useClubData(clubAddress);
 
-  const creditVaultContract = useCreditVaultContract(clubAddress);
+  const { symbol: clubTokenSymbol, raiseOver } = clubData;
 
   const action = tab === "mint" ? "Mint" : "Redeem";
-
-  const { assetBalance, clubTokenBalance } = clubMember;
-
-  const {
-    address: assetTokenAddress,
-    symbol: assetTokenSymbol,
-    decimals: assetTokenDecimals,
-  } = assetToken;
-
-  const {
-    raiseOver,
-    totalAssets,
-    initialRaise,
-    symbol: clubTokenSymbol,
-    decimals: clubTokenDecimals,
-    lockupPeriod,
-  } = clubData;
-
-  const {
-    sendTokenSymbol,
-    sendTokenBalance,
-    sendTokenDecimals,
-    receiveTokenSymbol,
-    receiveTokenDecimals,
-    receiveTokenAddress,
-  } = tab === "mint" ? {
-    sendTokenSymbol: assetTokenSymbol,
-    sendTokenBalance: assetBalance,
-    sendTokenDecimals: assetTokenDecimals,
-    receiveTokenSymbol: clubTokenSymbol,
-    receiveTokenDecimals: clubTokenDecimals,
-    receiveTokenAddress: clubAddress,
-  } : {
-    sendTokenSymbol: clubTokenSymbol,
-    sendTokenBalance: clubTokenBalance,
-    sendTokenDecimals: clubTokenDecimals,
-    receiveTokenSymbol: assetTokenSymbol,
-    receiveTokenDecimals: assetTokenDecimals,
-    receiveTokenAddress: assetTokenAddress,
-  };
-
-  const validate = (inputs: IFormValues) => {
-    const shares = inputs.shares as IFormField;
-
-    if (tab === "mint") {
-      if (shares.raw > sendTokenBalance) {
-        return `Only ${formatDecimals(sendTokenBalance, sendTokenDecimals)} ${sendTokenSymbol} Available`;
-      }
-    } else {
-      if (shares.raw > clubTokenBalance) {
-        return `Only ${formatDecimals(sendTokenBalance, sendTokenDecimals)} ${sendTokenSymbol} Available`;
-      }
-    }
-  };
-
-  const {
-    setRawValue,
-    register,
-    values = {},
-    errors = {},
-    empty
-  } = useForm({ decimals: sendTokenDecimals, validate });
-
-  const shares = values.shares as IFormField || empty;
-  const sharesRaw = shares.raw || 0n;
-
-  const { data: amountReceived } = useMintRedeemPreview({
-    action: tab,
-    shares: sharesRaw,
-    clubAddress,
-  });
-
-  const redeemButtonProps = useWrite({
-    ...creditVaultContract,
-    functionName: "redeem",
-    args: [sharesRaw, address, address],
-    label: sharesRaw <= 0n
-      ? "Enter an amount"
-      : `${action} ${formatDecimals(amountReceived, receiveTokenDecimals, 0)} ${receiveTokenSymbol}`,
-    disabled: !!errors.shares || sharesRaw <= 0n || clubTokenBalance < sharesRaw,
-    onComplete: async (hash: string) => {
-      close();
-      refetchClubData();
-      refetchClubMember();
-      openModal(POST_TX_MODAL, {
-        header: `Your redeem was successful`,
-        title: "Redeem",
-        content: <p className="font-mono mt-2">You successfully redeemed {formatDecimals(amountReceived, receiveTokenDecimals, 2, false)} {receiveTokenSymbol}</p>,
-        action: {
-          icon: WalletIcon,
-          label: "Add token to wallet",
-          onClick: () => watchAsset({
-            type: 'ERC20',
-            options: {
-              address: receiveTokenAddress,
-              symbol: receiveTokenSymbol,
-              decimals: receiveTokenDecimals,
-            },
-          })
-        },
-        hash,
-      })
-    }
-  });
-
-  const inputError = () => {
-    if (errors.shares) {
-      return errors.shares;
-    }
-
-    if (tab === "mint") {
-      if (totalAssets + sharesRaw > initialRaise) {
-        return "You cannot mint more than the initial raise";
-      }
-    }
-
-    return null;
-  };
 
   return (
     <ModalOverlay onClick={close}>
@@ -191,115 +48,10 @@ export const MintRedeemModal = ({
             ]}
           />
 
-          <Input
-            type="number"
-            name="amount"
-            label={`${action} Amount`}
-            rightLabel={`Max. ${formatDecimals(sendTokenBalance, sendTokenDecimals, 2)} ${sendTokenSymbol}`}
-            rightLabelAction={() => setRawValue("shares", sendTokenBalance)}
-            suffix={sendTokenSymbol}
-            placeholder="0.0"
-            className="mt-4"
-            value={shares.formatted}
-            onChange={register("shares")}
-            error={inputError()}
-            disabled={tab === "redeem" && locked}
-          />
-
-          <SendReceivePanel
-            className="my-4"
-            leftPanel={{
-              title: "What you send",
-              value: formatDecimals(sharesRaw, sendTokenDecimals, 2),
-              icon: ` ${sendTokenSymbol}`,
-            }}
-            rightPanel={{
-              title: "What you receive",
-              value: formatDecimals(amountReceived, receiveTokenDecimals, 2, false),
-              icon: ` ${receiveTokenSymbol}`
-            }}
-          />
-
-          {tab === "redeem" && (!activated || locked) && (
-            <InfoBanner
-              align="left"
-              variant="warning"
-              label={!activated ? "Redeem is not available until the club has been activated and the lock period has expired." : `Redeem is not available until the club locked period has expired. There are ${formatDuration(remaining)} remaining until the club is unlocked.`}
-              className="text-sm mt-4 p-3 bg-slate-50  font-mono border border-black"
-            />
-          )}
-
           {tab === "mint" ? (
-            <ApprovalButton
-              owner={address}
-              amount={sharesRaw}
-              disabled={!!inputError() || sharesRaw < 0n || assetBalance < sharesRaw}
-              spender={creditVaultContract.address}
-              tokenContract={{
-                abi: erc20Abi,
-                address: assetTokenAddress,
-              }}
-              actionProps={{
-                ...creditVaultContract,
-                functionName: "deposit",
-                args: [sharesRaw, address],
-                label: sharesRaw <= 0n
-                  ? "Enter an amount"
-                  : sharesRaw > assetBalance
-                    ? "Insufficient balance"
-                    : `${action} ${formatDecimals(amountReceived, receiveTokenDecimals, 2)} ${receiveTokenSymbol}`,
-                disabled: !!errors.shares || sharesRaw < 0n || assetBalance < sharesRaw,
-                onComplete: async (hash: string) => {
-                  refetchClubData();
-                  refetchClubMember();
-                  openModal(POST_TX_MODAL, {
-                    header: `Your mint was successful`,
-                    title: "Mint",
-                    content: (
-                      <>
-                        <p className="font-mono mt-2">You successfully minted {formatDecimals(amountReceived, receiveTokenDecimals, 2, false)} {receiveTokenSymbol}</p>
-
-                        <div className="flex items-center gap-2 my-4 text-sm text-blue-600">
-                          <CalendarIcon width={24} className="fill" />
-                          Redeemable: {activated
-                          ? locked
-                            ? formatDuration(remaining)
-                            : "Now"
-                          : formatDuration(Number(lockupPeriod))}
-                        </div>
-                      </>
-                    ),
-                    action: {
-                      icon: WalletIcon,
-                      label: "Add token to wallet",
-                      onClick: () => watchAsset({
-                        type: 'ERC20',
-                        options: {
-                          address: receiveTokenAddress,
-                          symbol: receiveTokenSymbol,
-                          decimals: receiveTokenDecimals,
-                        },
-                      })
-                    },
-                    hash,
-                  })
-                }
-              }}
-            />
+            <MintPanel clubAddress={clubAddress} />
           ) : (
-            <Button
-              fluid
-              className="mt-4"
-              color="primary"
-              size="large"
-              label={sharesRaw <= 0n
-                ? "Enter an amount"
-                : sharesRaw > clubTokenBalance
-                  ? "Insufficient balance"
-                  : `${action} ${formatDecimals(amountReceived, receiveTokenDecimals, 2)} ${receiveTokenSymbol}`}
-              {...redeemButtonProps}
-              disabled={!activated}
-            />
+            <RedeemPanel clubAddress={clubAddress} />
           )}
         </Modal.Body>
       </Modal>
---
+++ components/modals/panels/RedeemPanel.tsx
@@ -0,0 +1,210 @@
+import {
+  Button,
+  Input,
+  Usdc,
+  InfoBanner,
+  WalletIcon,
+  // @ts-ignore
+} from "@unioncredit/ui";
+
+import { useModals } from "@/providers/ModalManagerProvider";
+import { useClubData } from "@/hooks/useClubData";
+import { Address } from "viem";
+import { useAccount, useWatchAsset } from "wagmi";
+import { useClubMember } from "@/hooks/useClubMember";
+import { useErc20Token } from "@/hooks/useErc20Token";
+import { formatDecimals } from "@/lib/format";
+import { IFormField, IFormValues, useForm } from "@/hooks/useForm";
+import { useMintRedeemPreview } from "@/hooks/useMintRedeemPreview";
+import { useCreditVaultContract } from "@/hooks/useCreditVaultContract";
+import { useWrite } from "@/hooks/useWrite";
+import { useClubActivation } from "@/hooks/useClubActivation";
+import { formatDuration } from "@/lib/utils";
+import { POST_TX_MODAL } from "@/components/modals/PostTxModal";
+import Image from "next/image";
+import { createIpfsImageUrl } from "@/lib/links";
+import { useClubMemberNft } from "@/hooks/useClubMemberNft";
+
+export const RedeemPanel = ({
+  clubAddress,
+}: {
+  clubAddress: Address;
+}) => {
+
+  const { open: openModal, close } = useModals();
+  const { address } = useAccount();
+  const { data: clubData, refetch: refetchClubData } = useClubData(clubAddress)
+  const { data: clubMember, refetch: refetchClubMember } = useClubMember(address, clubAddress);
+  const { data: assetToken } = useErc20Token(clubData.assetAddress);
+  const { data: clubMemberNftData } = useClubMemberNft(clubAddress);
+  const { activated, locked, remaining } = useClubActivation(clubAddress);
+  const { watchAsset } = useWatchAsset();
+
+  const creditVaultContract = useCreditVaultContract(clubAddress);
+
+  const { clubTokenBalance } = clubMember;
+  const { image: ipfsImageLink } = clubMemberNftData;
+
+  const {
+    address: assetTokenAddress,
+    symbol: assetTokenSymbol,
+    decimals: assetTokenDecimals,
+  } = assetToken;
+
+  const {
+    symbol: clubTokenSymbol,
+    decimals: clubTokenDecimals,
+  } = clubData;
+
+  const {
+    sendTokenSymbol,
+    sendTokenBalance,
+    sendTokenDecimals,
+    receiveTokenSymbol,
+    receiveTokenDecimals,
+    receiveTokenAddress,
+  } = {
+    sendTokenSymbol: clubTokenSymbol,
+    sendTokenBalance: clubTokenBalance,
+    sendTokenDecimals: clubTokenDecimals,
+    receiveTokenSymbol: assetTokenSymbol,
+    receiveTokenDecimals: assetTokenDecimals,
+    receiveTokenAddress: assetTokenAddress,
+  };
+
+  const validate = (inputs: IFormValues) => {
+    const shares = inputs.shares as IFormField;
+
+    if (shares.raw > clubTokenBalance) {
+      return `Only ${formatDecimals(sendTokenBalance, sendTokenDecimals)} ${sendTokenSymbol} Available`;
+    }
+  };
+
+  const {
+    setRawValue,
+    register,
+    values = {},
+    errors = {},
+    empty
+  } = useForm({ decimals: sendTokenDecimals, validate });
+
+  const shares = values.shares as IFormField || empty;
+  const sharesRaw = shares.raw || 0n;
+
+  const { data: amountReceived } = useMintRedeemPreview({
+    action: "redeem",
+    shares: sharesRaw,
+    clubAddress,
+  });
+
+  const redeemButtonProps = useWrite({
+    ...creditVaultContract,
+    functionName: "redeem",
+    args: [sharesRaw, address, address],
+    label: sharesRaw <= 0n
+      ? "Enter an amount"
+      : `Redeem ${formatDecimals(amountReceived, receiveTokenDecimals, 0)} ${receiveTokenSymbol}`,
+    disabled: !!errors.shares || sharesRaw <= 0n || clubTokenBalance < sharesRaw,
+    onComplete: async (hash: string) => {
+      close();
+      refetchClubData();
+      refetchClubMember();
+      openModal(POST_TX_MODAL, {
+        header: `Your redeem was successful`,
+        title: "Redeem",
+        content: <p className="font-mono mt-2">You successfully
+          redeemed {formatDecimals(amountReceived, receiveTokenDecimals, 2, false)} {receiveTokenSymbol}</p>,
+        action: {
+          icon: WalletIcon,
+          label: "Add token to wallet",
+          onClick: () => watchAsset({
+            type: 'ERC20',
+            options: {
+              address: receiveTokenAddress,
+              symbol: receiveTokenSymbol,
+              decimals: receiveTokenDecimals,
+            },
+          })
+        },
+        hash,
+      })
+    }
+  });
+
+  const inputError = () => {
+    if (errors.shares) {
+      return errors.shares;
+    }
+
+    return null;
+  };
+
+  return (
+    <>
+      <Input
+        type="number"
+        name="amount"
+        label="Redeem Amount"
+        placeholder="0.0"
+        className="mt-4"
+        value={shares.formatted}
+        onChange={register("shares")}
+        error={inputError()}
+        disabled={locked}
+        suffix={(
+          <Image
+            width={24}
+            height={24}
+            src={createIpfsImageUrl(ipfsImageLink)}
+            alt="Fund Image"
+            className="border border-black"
+          />
+        )}
+        {...(!locked ? {
+          rightLabel: `Max. ${formatDecimals(sendTokenBalance, sendTokenDecimals, 2)} ${sendTokenSymbol}`,
+          rightLabelAction: () => setRawValue("shares", sendTokenBalance),
+        } : {})}
+      />
+
+      <h2 className="mt-4 mb-0.5">You receive:</h2>
+      <div className="bg-stone-100 p-1 rounded-2xl">
+        <Input
+          disabled={true}
+          placeHolder="0.0"
+          value={locked ? "" : formatDecimals(amountReceived, receiveTokenDecimals, 2, false)}
+          suffix={<Usdc />}
+        />
+      </div>
+
+      {(!activated || locked) ? (
+        <InfoBanner
+          align="left"
+          variant="warning"
+          label={!activated ? "Redeem is not available until the club has been activated and the lock period has expired." : `Redeem is not available until the club locked period has expired. There are ${formatDuration(remaining)} remaining until the club is unlocked.`}
+          className="text-sm mt-4 p-2 bg-slate-50  font-mono border border-black"
+        />
+      ) : (
+        <>
+        </>
+      )}
+
+      <Button
+        fluid
+        className="mt-4"
+        color="primary"
+        size="large"
+        label={!activated
+          ? "Club not activated"
+          : locked
+            ? `Locked for ${formatDuration(remaining)}`
+            : sharesRaw <= 0n
+              ? "Enter an amount"
+              : sharesRaw > clubTokenBalance
+                ? "Insufficient balance"
+                : `Redeem ${formatDecimals(amountReceived, receiveTokenDecimals, 2)} ${receiveTokenSymbol}`}
+        {...redeemButtonProps}
+        disabled={!activated}
+      />
+    </>
+  )
+}
