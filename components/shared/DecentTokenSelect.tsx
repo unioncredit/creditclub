fbd09e7286ecd867bbf9b70f3ee4576c5e7f8536
barbs <115395121+0xbarbs@users.noreply.github.com>
2025-03-27 14:01:44 +0000

    UNI-2349 Swap modal improvements #85

--- components/shared/DecentTokenSelect.tsx
+++ components/shared/DecentTokenSelect.tsx
@@ -3,31 +3,55 @@ import { Select, Skeleton, Union } from "@unioncredit/ui";
 import { useUsersBalances } from "@decent.xyz/box-hooks";
 import { useAccount } from "wagmi";
 import { base, optimism } from "viem/chains";
-import { zeroAddress } from "viem";
+import { Address, zeroAddress } from "viem";
 import { UserTokenInfo } from "@decent.xyz/box-common";
 
 import { DEFAULT_CHAIN_ID } from "@/constants";
+import { usdcContract } from "@/contracts/base";
+import { useEffect } from "react";
 
 export const DecentTokenSelect = ({
   onChange,
+  initialToken,
 }:{
   onChange: (token: UserTokenInfo) => void;
+  initialToken?: Address;
 }) => {
   const { address } = useAccount();
-
   const { tokens: allTokens = [], isLoading } = useUsersBalances({
     address,
     chainId: DEFAULT_CHAIN_ID,
     selectChains: [base.id, optimism.id],
   });
 
-  const tokens = allTokens.filter(t => t.balance > 0n || t.address === zeroAddress);
+  const tokens = allTokens.filter(
+    t => t.balance > 0n
+      || t.address === zeroAddress
+      || t.address === usdcContract.address
+      || t.address === initialToken
+  );
+
+  const options = tokens.map(token => ({
+    value: token.address,
+    label: token.symbol,
+    imageSrc: `${process.env.NEXT_PUBLIC_URL}/api/token-image?logo=${token.logo}&chainId=${token.chainId}`,
+  }));
+
+  useEffect(() => {
+    if (initialToken && onChange) {
+      const token = tokens.find(t => t.address === initialToken);
+      if (token) {
+        onChange(token)
+      }
+    }
+  }, [initialToken, JSON.stringify(tokens, (_, value) => (typeof value === "bigint" ? value.toString() : value))]);
 
   return isLoading ? (
     <Skeleton width={100} height={38} shimmer />
   ) : (
     <div className="DecentTokenSelect">
       <Select
+        defaultValue={options.find(t => t.value === initialToken)}
         onChange={(row: any) => {
           const token = tokens.find((t) => t.address === row.value);
           if (token) {
@@ -39,11 +63,7 @@ export const DecentTokenSelect = ({
         }}
         menuPlacement="auto"
         maxMenuHeight={150}
-        options={tokens.map(token => ({
-          value: token.address,
-          label: token.symbol,
-          imageSrc: `${process.env.NEXT_PUBLIC_URL}/api/token-image?logo=${token.logo}&chainId=${token.chainId}`,
-        }))}
+        options={options}
       />
     </div>
   )
