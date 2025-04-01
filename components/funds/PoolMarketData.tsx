0e3128fe01121edc70f2c37652a24edb29954149
Jacob Shiach <kingjacob@gmail.com>
2025-04-02 01:30:12 +0200

    fix volum decimals

--- components/funds/PoolMarketData.tsx
+++ components/funds/PoolMarketData.tsx
@@ -22,7 +22,7 @@ export const PoolMarketData = ({
     <div className={cn("flex items-center justify-between border border-black py-2 px-3 font-mono", className)}>
       <div>
         <h3 className="font-semibold">{symbol}/USDC Pool:</h3>
-        <p className="text-sm">24hr Volume: <span className="font-medium">${volume_24h}</span></p>
+        <p className="text-sm">24hr Volume: <span className="font-medium">${(volume_24h || 0).toFixed(2)}</span></p>
         <p className="text-sm">Price: <span className="font-medium">${(price || 0).toFixed(2)}</span></p>
       </div>
 
