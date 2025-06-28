8ce02d28a9e6d052ca2ff853a54a3eb32f7e10c2
Jacob Shiach <kingjacob@gmail.com>
2025-06-28 00:11:10 -0700

    Fix build errors and React error #310
    - Remove incompatible usePopulateEns/usePopulateFnames hooks from FundHoldersTable
      These hooks expect IContact[] but useHolders returns Holder[] type
    - Remove all console.log statements that could cause React error #310
      Cleaned up: fetchHolders, DecentSwapButton, MintMemberNftMultichain, useDecentSwap
    - Remove unused imports from MintNftModal (format, useToken)
    - Fix all TypeScript compilation errors

--- components/modals/MintNftModal.tsx
+++ components/modals/MintNftModal.tsx
@@ -10,8 +10,7 @@ import { useModals } from "@/providers/ModalManagerProvider";
 import { StatGrid, StatGridRow } from "@/components/shared/StatGrid";
 import { useClubData } from "@/hooks/useClubData";
 import { useNewMemberData } from "@/hooks/useNewMemberData";
-import { format, formatDecimals } from "@/lib/format";
-import { useToken } from "@/hooks/useToken";
+import { formatDecimals } from "@/lib/format";
 import { formatDuration } from "@/lib/utils";
 import { useErc20Token } from "@/hooks/useErc20Token";
 import { MintMemberNftMultichain } from "@/components/shared/MintMemberNftMultichain";
@@ -26,7 +25,6 @@ export const MintNftModal = ({
 }) => {
   const { close } = useModals();
   const { address } = useAccount();
-  const { token } = useToken();
   const { data: clubData } = useClubData(clubAddress);
   const { data: clubMemberNft } = useClubMemberNft(clubAddress);
   const { data: newMemberData } = useNewMemberData(address, clubAddress);
