b4d61307ff4fe7b64a2f6e54a7c775b01aa685fc
Jacob Shiach <kingjacob@gmail.com>
2025-07-03 05:51:32 -0700

    refactor: update SVG size in RoundedButton component
    - Adjust the size of SVG icons within the RoundedButton component from size-4 to size-6, enhancing visual consistency and improving the overall appearance of buttons.

--- components/ui/RoundedButton.tsx
+++ components/ui/RoundedButton.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "font-sans rounded-[10px] border inline-flex items-center justify-center gap-1 whitespace-nowrap text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "font-sans rounded-[10px] border inline-flex items-center justify-center gap-1 whitespace-nowrap text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-6 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
